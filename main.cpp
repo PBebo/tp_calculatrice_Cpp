b9d03d58459bec8d30239afcb382c70832bd5757
pierrickbobet <pierrick.bobet@cryptolab.net>
2015-01-30 21:55:14 +0100

    Ajout en cas d'erreur une boucle faire tant que

--- main.cpp
+++ main.cpp
@@ -8,26 +8,37 @@ int main()
 	char operateur;
 	
 	// Demande a l utilisateur, affichage
-	cout << "---\nCalculatrice C++ par Pierrick Bobet\n---\nChoisir votre opérateur (+, -, x, /, %) : ";
-	cin >> operateur;
+	do{
+		cout << "---\nCalculatrice C++ par Pierrick Bobet\n---\nChoisir votre opérateur (+, -, x, /, %) : ";
+		cin >> operateur;
+		cout << operateur;
+	} while (operateur != '+' && operateur != '-' && operateur != 'x' && operateur != '/' && operateur != '%');
 
 	cout << "---\nSaisir le premier entier de votre calcul : ";
 	cin >> entier1;
 
 	cout << "\nSaisir le deuxieme entier de votre calcul : ";
-	cin >> entier2
+	cin >> entier2;
 
 	// Calcul avec l operateur
-	
-	if (operateur == '+'){
-	entier1 = entier1 + entier2;
-	}
-
-	if else {operateur == '-'){
-		if (entier1 > entier2){
-			entier1 = entier1 - entier2;
+		// ADDITION	
+		if (operateur == '+'){
+		entier1 = entier1 + entier2;
 		}
-		else {
+		
+		// SOUSTRACTION
+		else if (operateur == '-'){
+			if (entier1 > entier2){
+				entier1 = entier1 - entier2;
+			}
+			else {
+				entier1 = entier2 - entier1;
+			}
+		}
+		// MULTIPLICATION
+		else if (operateur == 'x'){
+		}
+
 
 
 return 0;
