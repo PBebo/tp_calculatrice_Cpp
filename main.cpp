091813152621e36c89e6069c8518ca793112935c
pierrickbobet <pierrick.bobet@cryptolab.net>
2015-01-30 21:30:40 +0100

    Evolution du code, ajout de l'addition et soustraction

--- main.cpp
+++ main.cpp
@@ -4,15 +4,30 @@ using namespace std;
 int main()
 {
 	// Declaration des variables entiers
-	int addition, soustraction, multiplication, division, modulo;
-	
+	int addition, soustraction, multiplication, division, modulo, entier1, entier2;
+	char operateur;
 	
 	// Demande a l utilisateur, affichage
-	cout << "Calculatrice C++ par Pierrick Bobet\nChoisir votre opérateur (+, -, x, /, modulo) : ";
+	cout << "---\nCalculatrice C++ par Pierrick Bobet\n---\nChoisir votre opérateur (+, -, x, /, %) : ";
+	cin >> operateur;
+
+	cout << "---\nSaisir le premier entier de votre calcul : ";
+	cin >> entier1;
+
+	cout << "\nSaisir le deuxieme entier de votre calcul : ";
+	cin >> entier2
+
+	// Calcul avec l operateur
 	
+	if (operateur == '+'){
+	entier1 = entier1 + entier2;
+	}
 
-
-
+	if else {operateur == '-'){
+		if (entier1 > entier2){
+			entier1 = entier1 - entier2;
+		}
+		else {
 
 
 return 0;
