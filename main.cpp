092da6fbe525d67f40d9b66a541023b4561f2a07
pierrickbobet <pierrick.bobet@cryptolab.net>
2015-02-02 17:32:06 +0100

    Amelioration du fichier main.cpp et correction du modulo.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,36 @@
 #include "./division.cpp"
 #include "./modulo.cpp"
 
-//using namespace std;
+using namespace std;
 
 int main()
 {
-	double operandeA, operandeB;
-	// Recuperation des deux operandes
-	cout << "---\nSaisir le premier operande de votre calcul : ";
-	cin >> operandeA;
-	cout << "\nSaisir le deuxieme operande de votre calcul : ";
-	cin >> operandeB;
+	// Affichage titre Calculatrice C++ :
+	cout << "---\nCalculatrice C++\n---\n\n";
+	int16_t a=321, b=23; 
+
+	int32_t c=321, d=23;
+
+	float e=2324, f=25;
+
+	double g=67863, h=432;
+	
+	cout << "Valeur pour calcul : " << endl;
+	cout << "Valeur de  a : " << a << endl;
+	cout << "Valeur de  b : " << b << endl;
+	cout << "Valeur de  c : " << c << endl;
+	cout << "Valeur de  d : " << d << endl;
+	cout << "Valeur de  e : " << e << endl;
+	cout << "Valeur de  f : " << f << endl;
+	cout << "Valeur de  g : " << g << endl;
+	cout << "Valeur de  h : " << h << endl << endl;
+
+
+	cout <<	"Addition : " << ajouter(a,b) << endl;
+	cout << "Soustraction : " << soustraction(a,b) << endl;
+	cout << "Multiplication : " << multiplication(a,b) << endl;
+	cout << "Division : " <<division(a,b) << endl;
+	cout << "Modulo : " << modulo(a,b) << endl;
 
 	return 0;
 }
--- modulo.cpp
+++ modulo.cpp
@@ -9,7 +9,7 @@
 ///
 int32_t modulo(int32_t operandeA, int32_t operandeB)
 {
-	return operandeA / operandeB;
+	return operandeA % operandeB;
 }
 
 /// \brief Fonction de modulo de deux nombre de type int64_t
@@ -22,31 +22,5 @@ int32_t modulo(int32_t operandeA, int32_t operandeB)
 ///
 int64_t modulo(int64_t operandeA, int64_t operandeB)
 {
-	return operandeA / operandeB;
-}
-	
-/// \brief Fonction de modulo de deux nombre de type Float
-///
-///
-/// \param operandeA est un Float
-/// \param operandeB est un Float
-/// \return Le modulo des deux Float saisie par l'utilisateur.
-/// \sa modulo(double, double), modulo(int32_t, int32_t), modulo(int64_t, int64_t)
-///
-float modulo(float operandeA, float operandeB)
-{
-	return operandeA / operandeB;
-}
-	
-/// \brief Fonction de modulo de deux nombre de type Double
-///
-///
-/// \param operandeA est un Double
-/// \param operandeB est un Double
-/// \return Le modulo des deux Double saisie par l'utilisateur.
-/// \sa modulo(int32_t, int32_t), modulo(int64_t, int64_t), modulo(float, float)
-///	
-double modulo(double operandeA, double operandeB)
-{
-	return operandeA / operandeB;
+	return operandeA % operandeB;
 }
